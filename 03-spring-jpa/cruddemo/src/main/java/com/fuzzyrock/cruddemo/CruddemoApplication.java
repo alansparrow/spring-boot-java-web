1b3c0864e40f275df826daa7f854954b54ce4530
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-06 21:49:37 +0100

    Save student to DB

--- 03-spring-jpa/cruddemo/src/main/java/com/fuzzyrock/cruddemo/CruddemoApplication.java
+++ 03-spring-jpa/cruddemo/src/main/java/com/fuzzyrock/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.fuzzyrock.cruddemo;
 
+import com.fuzzyrock.cruddemo.dao.StudentDAO;
+import com.fuzzyrock.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,16 +10,29 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
-	// Executed after the Spring Beans have been loaded
-	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
-		return runner -> {
-			System.out.println("Hello World!");
-		};
-	}
+    // Executed after the Spring Beans have been loaded
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return runner -> {
+            createStudent(studentDAO);
+        };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        // Create the student object
+        System.out.println("Creating a new student object...");
+        Student student = new Student("Alan", "Sparrow", "alan@gmail.com");
+
+        // Save the student object
+        System.out.println("Saving new student...");
+        studentDAO.save(student);
+
+        // Display id of the saved student
+        System.out.println("Saved student. Generated id: " + student.getId());
+    }
 
 }
