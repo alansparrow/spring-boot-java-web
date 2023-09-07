b25677776ca90a98c229d4a00896cf9d670843f0
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-07 22:15:26 +0100

    99. Try Jackson for JSON<->POJO binding

---
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/fuzzyrock/restdemo/rest/StudentRestController.java
@@ -0,0 +1,24 @@
+package com.fuzzyrock.restdemo.rest;
+
+import com.fuzzyrock.restdemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Mario", "Luggi"));
+        students.add(new Student("Monkey", "Luffy"));
+        students.add(new Student("Sanji", "Macron"));
+
+        return students;
+    }
+}
