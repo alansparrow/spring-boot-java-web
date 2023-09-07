93e39b40672f736acaa7d2f1ecfd96ff9bcb7fb2
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-07 22:28:13 +0100

    Refactor load data

--- 04-spring-boot-rest-crud/restdemo/src/main/java/com/fuzzyrock/restdemo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/fuzzyrock/restdemo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.fuzzyrock.restdemo.rest;
 
 import com.fuzzyrock.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Mario", "Luggi"));
         students.add(new Student("Monkey", "Luffy"));
         students.add(new Student("Sanji", "Macron"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
