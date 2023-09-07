2b013497e662dcb377213fb1668569e881fff1f7
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-07 23:12:22 +0100

    Try multiple path variables

--- 04-spring-boot-rest-crud/restdemo/src/main/java/com/fuzzyrock/restdemo/rest/DemoRestController.java
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/fuzzyrock/restdemo/rest/DemoRestController.java
@@ -1,6 +1,7 @@
 package com.fuzzyrock.restdemo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class DemoRestController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/{firstName}/{lastName}")
+    public String sayHello(@PathVariable String firstName, @PathVariable String lastName) {
+        return firstName + " " + lastName;
     }
 }
