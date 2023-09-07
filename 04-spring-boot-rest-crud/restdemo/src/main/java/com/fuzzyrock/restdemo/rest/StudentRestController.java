04f4b1107776704b4195ba4707841413af4faaf3
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-07 22:48:47 +0100

    Handle exception

--- 04-spring-boot-rest-crud/restdemo/src/main/java/com/fuzzyrock/restdemo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restdemo/src/main/java/com/fuzzyrock/restdemo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.fuzzyrock.restdemo.rest;
 
 import com.fuzzyrock.restdemo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +30,19 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found: " + studentId);
+        }
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(404);
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
