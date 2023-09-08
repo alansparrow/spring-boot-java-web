8a1f8dc1d8a9c989e90164674dc53eee267b9af7
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-08 23:17:43 +0100

    Add rest api for all other CRUD

--- 04-spring-boot-rest-crud/cruddemo/src/main/java/com/fuzzyrock/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/cruddemo/src/main/java/com/fuzzyrock/cruddemo/rest/EmployeeRestController.java
@@ -2,9 +2,7 @@ package com.fuzzyrock.cruddemo.rest;
 
 import com.fuzzyrock.cruddemo.entity.Employee;
 import com.fuzzyrock.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,38 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found: " + employeeId);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        // Set id to 0 to force of a save of new entity (instead of update)
+        employee.setId(0);
+
+        Employee savedEmployee = employeeService.save(employee);
+
+        return savedEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.save(employee);
+
+        return savedEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable int employeeId) {
+        employeeService.deleteById(employeeId);
     }
 }
