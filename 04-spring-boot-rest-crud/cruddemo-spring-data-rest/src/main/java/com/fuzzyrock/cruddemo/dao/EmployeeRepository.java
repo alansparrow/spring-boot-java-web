6f46a57423f23f82b23666b13dded905c0cf8acb
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-09 00:59:34 +0100

    Try Spring Data Rest configs and sorting

--- 04-spring-boot-rest-crud/cruddemo-spring-data-rest/src/main/java/com/fuzzyrock/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/cruddemo-spring-data-rest/src/main/java/com/fuzzyrock/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,8 @@ package com.fuzzyrock.cruddemo.dao;
 
 import com.fuzzyrock.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // Set customized resource path
-// @RepositoryRestResource(path = "members")
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
