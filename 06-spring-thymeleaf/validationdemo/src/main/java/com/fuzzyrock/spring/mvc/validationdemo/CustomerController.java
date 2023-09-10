efdb2665178e21fa5b0ed8751ea63d77b035fa2e
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-10 21:20:31 +0100

    Add InitBinder for preprocessing request

--- 06-spring-thymeleaf/validationdemo/src/main/java/com/fuzzyrock/spring/mvc/validationdemo/CustomerController.java
+++ 06-spring-thymeleaf/validationdemo/src/main/java/com/fuzzyrock/spring/mvc/validationdemo/CustomerController.java
@@ -1,15 +1,25 @@
 package com.fuzzyrock.spring.mvc.validationdemo;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+  @InitBinder
+  public void initBinder(WebDataBinder webDataBinder) {
+    StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(/* emptyAsNull= */ true);
+    webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+  }
+
   @GetMapping("/")
   public String showForm(Model model) {
     model.addAttribute("customer", new Customer());
@@ -20,6 +30,8 @@ public class CustomerController {
   @PostMapping("/processForm")
   public String processForm(
       @Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+
+    System.out.println("Last name: |" + customer.getLastName() + "|");
     if (bindingResult.hasErrors()) {
       return "customer-form";
     } else {
