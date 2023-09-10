61a73cc03397f6c6d54db783c08bb43540663f8e
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-10 21:39:52 +0100

    Add regex validation

--- 06-spring-thymeleaf/validationdemo/src/main/java/com/fuzzyrock/spring/mvc/validationdemo/Customer.java
+++ 06-spring-thymeleaf/validationdemo/src/main/java/com/fuzzyrock/spring/mvc/validationdemo/Customer.java
@@ -1,9 +1,6 @@
 package com.fuzzyrock.spring.mvc.validationdemo;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
   private String firstName;
@@ -15,6 +12,17 @@ public class Customer {
   @Min(value = 1, message = "Min value is 1")
   @Max(value = 10, message = "Max value is 10")
   private int freePasses;
+
+  @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+  public String getPostalCode() {
+    return postalCode;
+  }
+
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
+  }
+
+  private String postalCode;
 
   public int getFreePasses() {
     return freePasses;
