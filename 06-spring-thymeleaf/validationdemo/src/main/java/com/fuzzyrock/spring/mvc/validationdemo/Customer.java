245dd3891100dc2a000e71b2a528ac467bae6c43
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-10 21:36:06 +0100

    Add min, max validation

--- 06-spring-thymeleaf/validationdemo/src/main/java/com/fuzzyrock/spring/mvc/validationdemo/Customer.java
+++ 06-spring-thymeleaf/validationdemo/src/main/java/com/fuzzyrock/spring/mvc/validationdemo/Customer.java
@@ -1,5 +1,7 @@
 package com.fuzzyrock.spring.mvc.validationdemo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,18 @@ public class Customer {
   @NotNull(message = "is required")
   @Size(min = 1, message = "is required")
   private String lastName;
+
+  @Min(value = 1, message = "Min value is 1")
+  @Max(value = 10, message = "Max value is 10")
+  private int freePasses;
+
+  public int getFreePasses() {
+    return freePasses;
+  }
+
+  public void setFreePasses(int freePasses) {
+    this.freePasses = freePasses;
+  }
 
   public String getFirstName() {
     return firstName;
