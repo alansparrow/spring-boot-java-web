bf3a2a4e3cf0eb53b2d9f47b4f21cd380042e9f1
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-10 21:14:27 +0100

    Try Spring Validation

---
+++ 06-spring-thymeleaf/validationdemo/src/test/java/com/fuzzyrock/spring/mvc/validationdemo/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuzzyrock.spring.mvc.validationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
