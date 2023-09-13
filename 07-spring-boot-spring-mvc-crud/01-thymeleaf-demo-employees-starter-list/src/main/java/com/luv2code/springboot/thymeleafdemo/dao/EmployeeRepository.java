3c75c3b9212e3154da737a212768ae2114530350
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-13 01:52:00 +0100

    Sort by last name

--- 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -4,8 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
+
+    // Spring Data JPA will parse the method name,
+    // and it will look for a specific format and pattern,
+    // then create appropriate query behind the scene.
+    public List<Employee> findAllByOrderByLastNameAsc();
 	
 }
