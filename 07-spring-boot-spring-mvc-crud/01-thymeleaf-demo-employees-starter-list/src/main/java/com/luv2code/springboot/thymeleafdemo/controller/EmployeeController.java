e7bf1b1ed0c200ece31da7aa94ec0bcd770fc34b
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-13 01:37:20 +0100

    CRUD DB Project Init

---
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,57 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		List<Employee> employees = employeeService.findAll();
+
+		// add to the spring model
+		theModel.addAttribute("employees", employees);
+
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		Employee employee = new Employee();
+
+		model.addAttribute("employee", employee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.save(employee);
+
+		return "redirect:/employees/list";
+	}
+}
+
+
+
+
+
+
+
+
+
