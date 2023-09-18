d1b6701860e73dbe3dcd6c3e77c0714c17c553b3
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-18 22:33:18 +0100

    Customize Users, Authorities to Members, Roles tables for authentication and authorization

--- 08-spring-boot-security/demosecurity/src/main/java/com/fuzzyrock/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-security/demosecurity/src/main/java/com/fuzzyrock/demosecurity/security/DemoSecurityConfig.java
@@ -1,24 +1,28 @@
 package com.fuzzyrock.demosecurity.security;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
   @Bean
   public UserDetailsManager userDetailsManager(DataSource dataSource) {
-    return new JdbcUserDetailsManager(dataSource);
+    JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+    jdbcUserDetailsManager.setUsersByUsernameQuery(
+        "select user_id, pw, active from members where user_id=?");
+
+    jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+        "select user_id, role from roles where user_id=?");
+
+    return jdbcUserDetailsManager;
   }
 
   //  @Bean
