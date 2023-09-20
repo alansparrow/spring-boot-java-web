bd832a4fab55cd2607cda5d8a7e61e940f1d1e73
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-20 17:24:04 +0100

    Add AppDAO

---
+++ 09-spring-hibernate/cruddemo2/src/main/java/com/fuzzyrock/cruddemo2/daos/AppDAO.java
@@ -0,0 +1,7 @@
+package com.fuzzyrock.cruddemo2.daos;
+
+import com.fuzzyrock.cruddemo2.entities.Instructor;
+
+public interface AppDAO {
+  void save(Instructor instructor);
+}
---
+++ 09-spring-hibernate/cruddemo2/src/main/java/com/fuzzyrock/cruddemo2/daos/AppDAOImpl.java
@@ -0,0 +1,22 @@
+package com.fuzzyrock.cruddemo2.daos;
+
+import com.fuzzyrock.cruddemo2.entities.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDAO {
+
+  private EntityManager entityManager;
+
+  @Autowired
+  public AppDAOImpl(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+
+  @Override
+  @Transactional
+  public void save(Instructor instructor) {
+    entityManager.persist(instructor);
+  }
+}
