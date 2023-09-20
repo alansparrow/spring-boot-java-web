b3188979e781ded10adb9af8e5956f0cd681f1ce
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-20 21:32:38 +0100

    Annotate Course entity class

--- 09-spring-hibernate/cruddemo2/src/main/java/com/fuzzyrock/cruddemo2/entities/Course.java
+++ 09-spring-hibernate/cruddemo2/src/main/java/com/fuzzyrock/cruddemo2/entities/Course.java
@@ -1,16 +1,22 @@
 package com.fuzzyrock.cruddemo2.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "course")
 public class Course {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
   private int id;
 
+  @Column(name = "title")
   private String title;
 
+  @ManyToOne(
+      cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+  @JoinColumn(name = "instructor_id")
   private Instructor instructor;
 
   public Course() {}
