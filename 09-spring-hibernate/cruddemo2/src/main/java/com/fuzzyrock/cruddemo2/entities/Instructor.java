b64cd7c3b5908a4d2ee2dafeed54559a1ec6d73b
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-20 17:09:40 +0100

    Add Instructor Entity

---
+++ 09-spring-hibernate/cruddemo2/src/main/java/com/fuzzyrock/cruddemo2/entities/Instructor.java
@@ -0,0 +1,100 @@
+package com.fuzzyrock.cruddemo2.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "email")
+  private String email;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "instructor_detail_id")
+  // instructor_detail_id defined in instructor table
+  // In DB, foreign key is configured to reference id field in instructor_detail table
+  //  `instructor_detail_id` int DEFAULT NULL,
+  //  PRIMARY KEY (`id`),
+  //  KEY `FK_DETAIL_idx` (`instructor_detail_id`),
+  //  CONSTRAINT `FK_DETAIL` FOREIGN KEY (`instructor_detail_id`) REFERENCES `instructor_detail`
+  // (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  private InstructorDetail instructorDetail;
+
+  public Instructor() {}
+
+  public Instructor(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public InstructorDetail getInstructorDetail() {
+    return instructorDetail;
+  }
+
+  public void setInstructorDetail(InstructorDetail instructorDetail) {
+    this.instructorDetail = instructorDetail;
+  }
+
+  @Override
+  public String toString() {
+    return "Instructor{"
+        + "id="
+        + id
+        + ", firstName='"
+        + firstName
+        + '\''
+        + ", lastName='"
+        + lastName
+        + '\''
+        + ", email='"
+        + email
+        + '\''
+        + ", instructorDetail="
+        + instructorDetail
+        + '}';
+  }
+}
