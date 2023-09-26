21fdd953b4ae1c11b4430e8b140458b27d48eb0a
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-26 23:52:28 +0100

    Match with full package, classname

--- 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-  @Before("execution(public void addAccount())")
+  @Before("execution(public void com.fuzzyrock.aopdemo.dao.MembershipDAOImpl.addAccount())")
   public void beforeAddAccountAdvice() {
     System.out.println("\n===> Executing @Before advice on addAccount()");
   }
