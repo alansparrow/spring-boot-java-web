599793a1907f5b4e3e5a337bb8adf80c7725e9b5
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-27 13:53:41 +0100

    Add order to aspects

--- 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.fuzzyrock.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
     @Before("com.fuzzyrock.aopdemo.aspect.CommonAspect.forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
--- 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,10 +2,12 @@ package com.fuzzyrock.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudLogAsyncAspect {
     @Before("com.fuzzyrock.aopdemo.aspect.CommonAspect.forDaoPackageNoGetterSetter()")
     public void logToCloudAsync() {
