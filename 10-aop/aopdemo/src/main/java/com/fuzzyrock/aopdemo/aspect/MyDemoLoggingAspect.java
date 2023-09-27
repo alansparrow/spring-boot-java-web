0a15c78bbad2881ee27df7c89dba8a16211b890d
Trung Tran <trannguyenbaotrung@gmail.com>
2023-09-27 01:30:14 +0100

    Declare and reuse Pointcut

--- 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-aop/aopdemo/src/main/java/com/fuzzyrock/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,14 +2,19 @@ package com.fuzzyrock.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+
+  @Pointcut("execution(* com.fuzzyrock.aopdemo.dao.*.*(..))")
+  private void forDaoPackage() {}
+
   // @Before("execution(public void add*())")
   // Match any class in the package, any method, any params
-  @Before("execution(* com.fuzzyrock.aopdemo.dao.*.*(..))")
+  @Before("forDaoPackage()")
   public void beforeAddAccountAdvice() {
     System.out.println("\n===> Executing @Before advice on addAccount()");
   }
